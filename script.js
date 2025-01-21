9a9e9e77f6e8ff336632366472ed3fbfc608e48d
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-01-21 21:46:32 +0530

    PrintQuotes() and remixArray() function added.

--- script.js
+++ script.js
@@ -11,3 +11,44 @@ function searchQuotes(txt) {
 
 
 console.log(Quotes);
+
+
+// function to print quotes
+function PrintQuotes(quotes){
+    const quotesDiv = document.getElementById('quotesList');
+    quotesDiv.innerHTML = '';
+    quotes.forEach(quote => {
+        const quoteDiv = document.createElement('div');
+        quoteDiv.classList.add('quote');
+        quoteDiv.innerHTML = `
+            <i class="fa-solid fa-quote-left lft_icn"></i>
+        
+            <div class="quote-category">
+                <hr>
+                <h3>${quote.category}</h3>
+                <hr>
+            </div>
+            <div class="quote-text">
+                <p>"${quote.quote}"</p>
+            </div>
+        
+            <div class="quote-author">
+                <p>- ${quote.author}</p>
+            </div>
+        `;
+        quotesDiv.appendChild(quoteDiv);
+
+ } );
+};
+
+// to remixArray
+function remixArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+   }
+// printing quotes
+const printIt = remixArray(Quotes);
+PrintQuotes(printIt.slice(0, 8));
