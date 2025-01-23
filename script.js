506cf8e574aaeea8da6103c2e9a54f956c97ad44
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-01-23 17:11:18 +0530

    filter by category function added.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // importing Quotes from data.js file
 import Quotes from './data.js';
 
+// GLOBAL VARIABLE 
+let CURRENT_QUOTES = [];
+
 // Input Search Quotes Function
 function searchQuotes(txt) {
     return Quotes.filter(item =>
@@ -8,22 +11,48 @@ function searchQuotes(txt) {
     )
 }
 
+// ----------------------------------------------
+//     function to handle selection of category
+// ---------------------------------------------- 
+function searchQuotesByCategory(category) {
+    return Quotes.filter(item => item.category === category);
+}
+
 // --------------------------------------
 const searchBtn = document.querySelector("#searchBtn");
-searchBtn.addEventListener("click", function(){
+searchBtn.addEventListener("click", function () {
+    CURRENT_QUOTES.length  = 0;
     // console.log("clicked -", this);
 
-    let searchInputTxt = document.querySelector('#searchInput').value ;
+    let searchInputTxt = document.querySelector('#searchInput').value;
     // console.log(searchInput.value.trim().toLocaleLowerCase());
 
-  let currentQuotes =  searchQuotes(searchInputTxt);
-//   console.log(currentQuotes)
-    PrintQuotes(currentQuotes);
+    let currentQuotes = searchQuotes(searchInputTxt);
+    CURRENT_QUOTES = [...currentQuotes];
+
+    // console.log("from search input function", CURRENT_QUOTES);
+    // PrintQuotes(currentQuotes);
 })
 
+// ----------------------------------------------
+//     function to handle selection of category
+// ---------------------------------------------- 
+const searchSelection = document.querySelector("#searchSelection");
+searchSelection.addEventListener("change", function () {
+  // to filter
+    let currentQuotes = searchQuotesByCategory(this.value);
 
-// function to print quotes
-function PrintQuotes(quotes){
+    // console.log("from category selection function", currentQuotes);
+
+    PrintQuotes(currentQuotes);
+    // console.log(this.value);
+});
+
+// ---------------------------------------------- 
+//            function to print quotes
+// ---------------------------------------------- 
+
+function PrintQuotes(quotes) {
     const quotesDiv = document.getElementById('quotesList');
     quotesDiv.innerHTML = '';
     quotes.forEach(quote => {
@@ -47,17 +76,17 @@ function PrintQuotes(quotes){
         `;
         quotesDiv.appendChild(quoteDiv);
 
- } );
+    });
 };
 
 // to remixArray
 function remixArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
-   }
+}
 // printing quotes
 const printIt = remixArray(Quotes);
 PrintQuotes(printIt.slice(0, 8));
